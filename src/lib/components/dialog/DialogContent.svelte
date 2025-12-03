d095504fe265ece03efb2eb475799d3de7cbfd23
ian0x-S2 <lucasian3003@gmail.com>
2025-12-03 18:52:51 -0300

    add svg icon

--- src/lib/components/dialog/DialogContent.svelte
+++ src/lib/components/dialog/DialogContent.svelte
@@ -69,7 +69,21 @@
      onclick={root.close}
      class="absolute top-4 right-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:ring-2 focus:ring-ring focus:ring-offset-2 focus:outline-none disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
     >
-     <span class="h-4 w-4">X</span>
+     <span class="h-4 w-4">
+      <svg
+       xmlns="http://www.w3.org/2000/svg"
+       width="20"
+       height="20"
+       viewBox="0 0 24 24"
+       fill="none"
+       stroke="currentColor"
+       stroke-width="2"
+       stroke-linecap="round"
+       stroke-linejoin="round"
+       class="lucide lucide-x-icon lucide-x"
+       ><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg
+      >
+     </span>
      <span class="sr-only">Close</span>
     </button>
    </div>
