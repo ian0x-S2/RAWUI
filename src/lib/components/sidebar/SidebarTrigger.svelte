f25d9e9449f7e9516e470b923a8732170f6d1d01
ian0x-S2 <lucasian3003@gmail.com>
2025-12-11 19:09:45 -0300

    change default icon

--- src/lib/components/sidebar/SidebarTrigger.svelte
+++ src/lib/components/sidebar/SidebarTrigger.svelte
@@ -39,11 +39,8 @@
    stroke-width="2"
    stroke-linecap="round"
    stroke-linejoin="round"
-   class="transition-transform duration-200"
+   class="lucide lucide-panel-left-icon lucide-panel-left"
+   ><rect width="18" height="18" x="3" y="3" rx="2" /><path d="M9 3v18" /></svg
   >
-   <line x1="3" y1="6" x2="21" y2="6"></line>
-   <line x1="3" y1="12" x2="21" y2="12"></line>
-   <line x1="3" y1="18" x2="21" y2="18"></line>
-  </svg>
  {/if}
 </button>
